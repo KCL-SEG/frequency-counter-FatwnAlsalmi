b617b3d14e3595af8593315d3f15e142db238641
FatwnAlsalmi <127515938+FatwnAlsalmi@users.noreply.github.com>
2023-10-04 13:44:48 +0100

    Update frequencies.py

--- frequencies.py
+++ frequencies.py
@@ -4,4 +4,11 @@
 def frequencies(items):
     frequencies = {}
     # Your code goes here
+    for value in items:
+        count = 0
+        for val in items:
+            if (value == val)
+                count+=1
+        frequencies[value] = count
+        
     return frequencies
